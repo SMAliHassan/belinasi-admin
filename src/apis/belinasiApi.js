37e9958cdb1af23834de20c27c75f593cf8ca7c5
SMAliHassan <syedalihassan6651@GMAIL.COM>
2022-08-23 22:51:17 +0500

    Added axios base for production

--- src/apis/belinasiApi.js
+++ src/apis/belinasiApi.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+// For PRODUCTION
+// export default axios.create({
+//   baseURL: "/api/v1",
+// });
+
+// For DEVELOPMENT
 export default axios.create({
   baseURL: "http://127.0.0.1:8000/api/v1",
   withCredentials: true,
